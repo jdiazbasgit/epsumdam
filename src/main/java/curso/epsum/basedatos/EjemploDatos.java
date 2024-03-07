7ddedd3e332fffe509c6bf2d5c27c395cf47d011
Xinjiepan <xinjie.pan@estudiante.epsum.school>
2024-03-07 10:14:55 +0100

    No se que he cambiado

--- src/main/java/curso/epsum/basedatos/EjemploDatos.java
+++ src/main/java/curso/epsum/basedatos/EjemploDatos.java
@@ -1,4 +1,4 @@
-package curso.epsum.basedatos;
+package main.java.curso.epsum.basedatos;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
--- src/main/java/curso/epsum/basedatos/entidades/DatoLaboral.java
+++ src/main/java/curso/epsum/basedatos/entidades/DatoLaboral.java
@@ -1,4 +1,4 @@
-package curso.epsum.basedatos.entidades;
+package main.java.curso.epsum.basedatos.entidades;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
