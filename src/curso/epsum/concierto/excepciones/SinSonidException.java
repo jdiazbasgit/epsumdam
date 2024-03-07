581d162b82f69f81a787aa7cc8f4a929fdcfc7a9
jdiazbasgit <git@studiotapal.es>
2024-03-07 09:01:22 +0100

    conflicto entr ramas

---
+++ src/curso/epsum/concierto/excepciones/SinSonidException.java
@@ -0,0 +1,8 @@
+package curso.epsum.concierto.excepciones;
+
+public class SinSonidException extends Exception {
+
+	public void devolverDinero() {
+		System.out.println("señores el instrumentio se ha rotyo, paasen por taquilla y les devolvemos el dinero");
+	}
+}
--- src/epsum/corso/git/Conflicto.java
+++ src/epsum/corso/git/Conflicto.java
@@ -2,62 +2,6 @@ package epsum.corso.git;
 
 public class Conflicto {
 
-    //soy DaniG
-
-
-    // soy Bo
-    // Soy Rodrigo
-    // soy emily
-    // soy Jorge Ortega Corral
-
-    //llados
-    //Soy Antonio
-    // soy Camila Ferreyra Navarro
-
-<<<<<<< HEAD
-
-
-
-    //Soy Antonio
-=======
-    // Soy Sihao
->>>>>>> 9a8a8bf5049923d7d7a0ad53389a93ceb102ab4d
-
-    // Soy Antonio
-
-    // soy emily
-
-    // soy Daniel Moral Garcia
-
-    // soy profe
-
-    // Soy Rodrigo
-    // soy emily
-
-<<<<<<< HEAD
-
-// soy Daniel Moral Garcia
-=======
-    // soy Jorge Ortega Corral
->>>>>>> 9a8a8bf5049923d7d7a0ad53389a93ceb102ab4d
-
-    // soy Enrique
-
-    // soy Enrique
-
-    // soy Axel Rojas Pérez
-    // soy Enrique
-    // soy Xinjie Pan
-    // soy Federico
-    // soy profe
-    // soy marlene
-    // soy Axel Rojas Pérez
-    // soy Enrique
-    // soy Xinjie Pan
-    // soy Federico
-    // soy profe
-    // soy marlene
-
-//Soy Juan Fernando Restrepo
+    //soy el profe
 
 }
