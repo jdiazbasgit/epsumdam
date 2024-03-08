3c390855a92c2d0f71e713cfca82f7f5c81f96bc
Your Name <you@example.com>
2024-03-08 12:13:44 +0100

    hice un cambio en el cod

---
+++ src/main/java/curso/epsum/basedatos/entidades/EstadoCivil.java
@@ -0,0 +1,37 @@
+package curso.epsum.basedatos.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "estadosciviles")
+public class EstadoCivil {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
+	private int id;
+	
+	private String descripcion;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+}
