b744b1f28609dd53160a10edaa7bffb96360f114
Rodrigo <rdc.1985@gmail.com>
2024-03-08 11:40:49 +0100

    agregue cargo empresa y estado civil

---
+++ src/main/java/curso/epsum/basedatos/entidades/Empresa.java
@@ -0,0 +1,45 @@
+package curso.epsum.basedatos.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "empresas")
+public class Empresa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	private String nombre;
+
+	private String cif;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCif() {
+		return cif;
+	}
+
+	public void setCif(String cif) {
+		this.cif = cif;
+	}	
+
+}
