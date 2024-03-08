d7e486720381ef26bb78a90ff29eff9778663ab7
DaniGN10 <daniel.galan@estudiante.epsum.school>
2024-03-08 13:06:24 +0100

    Actualizado

--- src/main/java/curso/epsum/basedatos/entidades/Cargo.java
+++ src/main/java/curso/epsum/basedatos/entidades/Cargo.java
@@ -1,5 +1,34 @@
 package curso.epsum.basedatos.entidades;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cargos")
 public class Cargo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String descripcion;
+	
+	public int getid() {
+		return id;
+	}
+
+	public void setid(int id) {
+		this.id = id;
+	}
+	public String getdescripcion() {
+		return descripcion;
+	}
+
+	public void setdescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
 
 }
--- src/main/java/curso/epsum/basedatos/entidades/EstadoCivil.java
+++ src/main/java/curso/epsum/basedatos/entidades/EstadoCivil.java
@@ -1,5 +1,31 @@
 package curso.epsum.basedatos.entidades;
 
-public class EstadoCivil {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "EstadosCiviles")
+public class EstadoCivil {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String descripcion;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 }
+	
