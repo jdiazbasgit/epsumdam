9fc7b2ca6650ac0518e2dc96f1f11be77afccb34
OsKaRT10 <oscargarmay4@gmail.com>
2024-03-08 13:21:44 +0100

    2 clases de base de datos

---
+++ src/main/java/curso/epsum/basedatos/entidades/Cargo.java
@@ -0,0 +1,35 @@
+package curso.epsum.basedatos.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cargos")
+
+public class Cargo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+
+	private String nombre;
+	private int id;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+
+
+	}
