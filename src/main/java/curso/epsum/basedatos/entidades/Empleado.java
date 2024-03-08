6810371fd3ca59d3dbb1496057c3b46cbe4e89a3
Axel Rojas <axelperez221105@gmail.com>
2024-03-08 13:35:53 +0100

    cambios finales entidades

---
+++ src/main/java/curso/epsum/basedatos/entidades/Empleado.java
@@ -0,0 +1,100 @@
+package curso.epsum.basedatos.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="empleados")
+public class Empleado {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int idEmpleado;
+	private String nombre;
+	private String DNI;
+	private String email;
+	private String tlf;
+	
+	@ManyToOne
+	@JoinColumn(name="idEmpresa")
+	private Empresa empresa;
+	
+	@ManyToOne
+	@JoinColumn(name="idDatosPersonal")
+	private DatoPersonal datosPersonal;
+	
+	@ManyToOne
+	@JoinColumn(name="datoLaboral")
+	private DatoLaboral datoLaboral;
+	
+	public int getIdEmpleado() {
+		return idEmpleado;
+	}
+
+	public void setIdEmpleado(int idEmpleado) {
+		this.idEmpleado = idEmpleado;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDNI() {
+		return DNI;
+	}
+
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTlf() {
+		return tlf;
+	}
+
+	public void setTlf(String tlf) {
+		this.tlf = tlf;
+	}
+
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+
+	public DatoPersonal getDatosPersonal() {
+		return datosPersonal;
+	}
+
+	public void setDatosPersonal(DatoPersonal datosPersonal) {
+		this.datosPersonal = datosPersonal;
+	}
+
+	public DatoLaboral getDatoLaboral() {
+		return datoLaboral;
+	}
+
+	public void setDatoLaboral(DatoLaboral datoLaboral) {
+		this.datoLaboral = datoLaboral;
+	}
+
+
+}
