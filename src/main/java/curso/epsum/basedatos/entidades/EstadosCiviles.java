93e6bd1f486f6b32919f8f82c32614ce741e6dfd
ingjuanfer <152220571+ingjuanfer@users.noreply.github.com>
2024-03-08 11:38:00 +0100

    ACTUALIZACION 1

--- src/main/java/curso/epsum/basedatos/entidades/EstadosCiviles.java
+++ src/main/java/curso/epsum/basedatos/entidades/EstadosCiviles.java
@@ -1,7 +1,35 @@
 package curso.epsum.basedatos.entidades;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "estados_civiles")
 public class EstadosCiviles {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int ID;
+	
+	private String DECRIPCION;
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getDECRIPCION() {
+		return DECRIPCION;
+	}
+
+	public void setDECRIPCION(String dECRIPCION) {
+		DECRIPCION = dECRIPCION;
+	}
+	
 }
