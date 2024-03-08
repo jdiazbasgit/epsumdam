eac0d1334985eb9326dfcc4561f0ee959f6fcf3a
Your Name <you@example.com>
2024-03-08 13:08:29 +0100

    cambios realizados

---
+++ src/main/java/curso/epsum/basedatos/entidades/Cargo.java
@@ -0,0 +1,36 @@
+package curso.epsum.basedatos.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Cargos")
+public class Cargo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String descripcion;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+
+}
