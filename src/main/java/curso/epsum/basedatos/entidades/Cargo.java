b6f687b77fa0e2afc9c343c0dc3108f36d061c5f
Your Name <you@example.com>
2024-03-12 08:57:38 +0100

    cambios

--- src/main/java/curso/epsum/basedatos/entidades/Cargo.java
+++ src/main/java/curso/epsum/basedatos/entidades/Cargo.java
@@ -30,6 +30,7 @@ public class Cargo {
 	}
 
 	public void setDescripcion(String descripcion) {
+		
 		this.descripcion = descripcion;
 	}
 }
--- src/main/java/curso/epsum/basedatos/entidades/DatoLaboral.java
+++ src/main/java/curso/epsum/basedatos/entidades/DatoLaboral.java
@@ -1,5 +1,50 @@
 package curso.epsum.basedatos.entidades;
 
-public class DatoLaboral {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import jakarta.annotation.Generated;
+
+@Entity
+@Table(name = "datos_laborales")
+public class DatoLaboral {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private int salario;
+	
+	@ManyToOne
+	//@JoinColumn(name = "idCargo")
+	private Cargo cargo;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getSalario() {
+		return salario;
+	}
+
+	public void setSalario(int salario) {
+		this.salario = salario;
+	}
+
+	public Cargo getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(Cargo cargo) {
+		this.cargo = cargo;
+	}
+	
 }
