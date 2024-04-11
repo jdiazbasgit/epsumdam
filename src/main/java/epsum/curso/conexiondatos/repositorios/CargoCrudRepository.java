2a5abc0dee9fa30a783e1d764ef2653ec641f274
Antonio <antonio.adolfo.jimenez@estudiante.epsum.school>
2024-04-11 10:03:55 +0200

    Repositorios y Servicios.

--- src/main/java/epsum/curso/conexiondatos/repositorios/CargoCrudRepository.java
+++ src/main/java/epsum/curso/conexiondatos/repositorios/CargoCrudRepository.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import epsum.curso.conexiondatos.entidades.Hijo;
+import epsum.curso.conexiondatos.entidades.Cargo;
 
 @Repository
-public interface HjiosCrudRepository extends CrudRepository<Hijo, Integer> {
+public interface CargoCrudRepository extends CrudRepository<Cargo, Integer> {
 
 	//@Query(value="SELECT id,descripcion FROM empresas.cargos where descripcion like 'M%'",nativeQuery = true)
 	//@Query("from Cargo as cargo where cargo.descripcion like :dato")
