c19758e00df9305a5d876b4d4103c9c74788cfd3
Axel Rojas <axelperez221105@gmail.com>
2024-04-16 13:22:55 +0200

    cambios entidades nombres

--- src/main/java/epsum/curso/conexiondatos/entidades/Empleado.java
+++ src/main/java/epsum/curso/conexiondatos/entidades/Empleado.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -28,10 +29,13 @@ public class Empleado
 	private String telefono;
 	
 	@ManyToOne
+	@JoinColumn(name="empresas_id")
 	private Empresa empresa;
 	@ManyToOne
+	@JoinColumn(name="datos_laborales_id")
 	private DatoLaboral datoLaboral;
 	@ManyToOne
+	@JoinColumn(name="datos_personales_id")
 	private DatoPersonal datoPersonal;
 	
 
