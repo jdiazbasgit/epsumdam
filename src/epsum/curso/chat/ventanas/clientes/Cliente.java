826f0af380ac8d0c2d970563df5253b2211b5c3f
marlenepaper <marlenepaper@hotmail.com>
2024-04-18 09:15:27 +0200

    a

--- src/epsum/curso/chat/ventanas/clientes/Cliente.java
+++ src/epsum/curso/chat/ventanas/clientes/Cliente.java
@@ -7,9 +7,9 @@ public class Cliente {
 
 	public static void main(String[] args) {
 
-		try (Socket socket= new Socket("192.168.0.125",8000)) {
+		try (Socket socket= new Socket("192.168.0.195",8000)) {
 			PrintWriter printWriter= new PrintWriter(socket.getOutputStream());
-			printWriter.println("eres el mas guapo");
+			printWriter.println("❤");
 			printWriter.flush();
 		} catch (Exception e) {
 			e.printStackTrace();
