470c407da16ac02f985dda4684997d404a7321b6
Antonio <antonio.adolfo.jimenez@estudiante.epsum.school>
2024-04-23 10:15:30 +0200

    PanelHijos

--- src/main/java/epsum/curso/conexiondatos/ventanas/PanelHijos.java
+++ src/main/java/epsum/curso/conexiondatos/ventanas/PanelHijos.java
@@ -1,5 +1,28 @@
 package epsum.curso.conexiondatos.ventanas;
 
-public class PanelHijos {
+public class PanelHijos  extends PanelComponente{
+
+	public PanelHijos(String[] cabeceras, Object[][] datos, String titulo) {
+		super(cabeceras, datos, titulo);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void alta() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void baja() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificar() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
