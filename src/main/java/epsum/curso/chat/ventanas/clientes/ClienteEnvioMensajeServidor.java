cbb1c49f46c02d3f6e70a45b0895f31a6225ebb5
Your Namecls <you@example.com>
2024-04-24 09:15:02 +0200

    logica mensajes

---
+++ src/main/java/epsum/curso/chat/ventanas/clientes/ClienteEnvioMensajeServidor.java
@@ -0,0 +1,25 @@
+package epsum.curso.chat.ventanas.clientes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import lombok.Data;
+
+@Data
+public class ClienteEnvioMensajeServidor extends ClienteChat {
+
+	private String mensajeSalida;
+	
+	public ClienteEnvioMensajeServidor(String ip, int puerto, String mensajeSalida) {
+		super(ip, puerto);
+		this.mensajeSalida=mensajeSalida;
+	}
+	@Override
+	public void hacerAlgo() throws IOException {
+		PrintWriter printWriter= new PrintWriter(getSocket().getOutputStream());
+		printWriter.println(getMensajeSalida());
+		printWriter.flush();
+
+	}
+
+}
