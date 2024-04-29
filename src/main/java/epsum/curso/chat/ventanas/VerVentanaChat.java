e4894920ad876a41e53c5832848d52c01275068e
Adrian Zambrano <adrianzambranomontero@gmail.com>
2024-04-29 10:15:31 +0200

    fdh

--- src/main/java/epsum/curso/chat/ventanas/VerVentanaChat.java
+++ src/main/java/epsum/curso/chat/ventanas/VerVentanaChat.java
@@ -17,9 +17,7 @@ public class VerVentanaChat {
 		ServidorEscuchaMensajeCliente servidorMensaje= new ServidorEscuchaMensajeCliente(ClienteChat.PUERTO_EXCUCHA_CLIENTE_MENSAJE, ventana);
 		servidorMensaje.start();
 		
-		ServidorEscuchaMensajeCliente servidorEscuchaMensajeCliente= 
-				new  ServidorEscuchaMensajeCliente(ClienteChat.PUERTO_EXCUCHA_CLIENTE_MENSAJE, ventana);
-		servidorEscuchaMensajeCliente.start();
+		
 		
 		
 	}
