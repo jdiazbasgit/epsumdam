f7db4f1cbb99185fa905fa230ec1c8f1a4b2c5c1
Your Name <you@example.com>
2024-04-30 09:23:57 +0200

    a

--- src/test/java/epsum/curso/conexiondatos/TestCargos.java
+++ src/test/java/epsum/curso/conexiondatos/TestCargos.java
@@ -4,6 +4,7 @@ package epsum.curso.conexiondatos;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -18,7 +19,7 @@ public class TestCargos {
 	private CargoService cargoService;
 	
 	
-	//@Test
+	@Test
 	public void alta() {
 		System.setProperty("java.awt.headless", "false");
 		Cargo cargo= new Cargo();
@@ -29,7 +30,7 @@ public class TestCargos {
 		assertFalse(getCargoService().existById(id));
 	}
 	
-	//@Test
+	@Test
 	public void count() {
 		assertNotEquals(getCargoService().count(), 0);
 	}
