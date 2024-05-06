f3178192a5be0ad9e6b2b962d2229e361d8c155f
Your Namecls <you@example.com>
2024-05-06 08:43:13 +0200

    configuration

--- src/main/java/epsum/curso/conexiondatos/ventanas/PanelCargos.java
+++ src/main/java/epsum/curso/conexiondatos/ventanas/PanelCargos.java
@@ -8,15 +8,15 @@ import lombok.Data;
 
 @Data
 public class PanelCargos extends PanelComponente {
+	@Autowired
+	private CargoService cargoService;
 
-	//private CargoService cargoService;
-	
 	private Object[] cabeceras;
-	
+
 	private Object[][] datos;
-	
+
 	private String titulo;
-	
+
 	public PanelCargos(Object[] cabeceras, Object[][] datos, String titulo) {
 		super(cabeceras, datos, titulo);
 	}
@@ -30,11 +30,10 @@ public class PanelCargos extends PanelComponente {
 	@Override
 	public void baja() {
 		int id = Integer.parseInt((String) getTabla().getModel().getValueAt(getTabla().getSelectedRow(), 0));
-		
-		System.err.println(id);
-		
-		
 
+		getCargoService().deleteById(id);
+		getTabla().removeAll();
+		
 	}
 
 	@Override
