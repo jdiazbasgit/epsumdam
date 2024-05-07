5c3c68c7d39c3e806a29e43547ae597ab53ccd8d
Your Name <you@example.com>
2024-05-07 12:38:44 +0200

    arreglo

--- src/main/java/epsum/curso/conexiondatos/ventanas/VentanaDatos.java
+++ src/main/java/epsum/curso/conexiondatos/ventanas/VentanaDatos.java
@@ -61,11 +61,9 @@ public class VentanaDatos extends JFrame implements WindowListener, ActionListen
 	@Autowired
 	private PanelCargos panelCargos;
 	@Autowired
-<<<<<<< HEAD
 	private PanelEmpleado panelEmpleado;
-=======
+	
 	private PanelHijos panelHijos;
->>>>>>> origin/springjpa
 	
 	private boolean primeraVez;
 	private JMenuBar menuBar;
