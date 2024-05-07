046ee263287ac1a4aff9531a75c7a558b7f45c1b
Adrian Zambrano <adrianzambranomontero@gmail.com>
2024-05-07 10:24:58 +0200

    Merge branch 'springjpa-hijos' of https://github.com/jdiazbasgit/epsumdam.git into springjpa-hijos

--- src/main/java/epsum/curso/conexiondatos/configuracion/ConfiguraciónHijos.java
+++ src/main/java/epsum/curso/conexiondatos/configuracion/ConfiguraciónHijos.java
@@ -15,5 +15,29 @@ import lombok.Data;
 @Configuration
 public class ConfiguraciónHijos {
 
+	@Autowired
+	private HijoService hijoService;
 	
+	public Object[] cabecerasHijos() {
+		Object[] cabeceras = {"ID", "CHICOS", "CHICAS"};
+		return cabeceras;
+	}
+	public Object[][] datosHijos(){
+		List<Hijo> hijos = (List<Hijo>) getHijoService().findAll();
+		Object[][] datos = new Object[(int) getHijoService().count()][3];
+		int i = 0;
+		for (Hijo c : hijos) {
+			datos[i][0] = String.valueOf(c.getId());
+			datos[i][1] = String.valueOf(c.getChicos());
+			datos[i][2] = String.valueOf(c.getChicas());
+			i++;
+		}
+		return datos;
+		
+	}
+	
+	@Bean
+	public PanelHijos getPanelHijos() {
+		return new PanelHijos(cabecerasHijos(), datosHijos(), "HIJOS");
+	}
 }
