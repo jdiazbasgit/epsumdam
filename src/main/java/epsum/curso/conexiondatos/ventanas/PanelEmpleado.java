80510cec171d2e30c4661e19a638c99cd1c0ec5c
Your Name <you@example.com>
2024-05-09 10:51:16 +0200

    Casi terminado el alta, baja terminado y modificar en proceso

--- src/main/java/epsum/curso/conexiondatos/ventanas/PanelEmpleado.java
+++ src/main/java/epsum/curso/conexiondatos/ventanas/PanelEmpleado.java
@@ -17,10 +17,15 @@ import javax.swing.JTextField;
 import javax.swing.border.Border;
 import javax.swing.table.DefaultTableModel;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import epsum.curso.conexiondatos.servicios.DatoLaboralService;
+import epsum.curso.conexiondatos.servicios.DatosPersonalesService;
 import epsum.curso.conexiondatos.servicios.EmpleadoService;
 import epsum.curso.conexiondatos.entidades.Empleado;
 import epsum.curso.conexiondatos.entidades.Empresa;
 import epsum.curso.conexiondatos.entidades.DatoLaboral;
+import epsum.curso.conexiondatos.entidades.DatoPersonal;
 import epsum.curso.conexiondatos.servicios.EmpresaService;
 import lombok.Data;
 @Data
@@ -28,9 +33,20 @@ public class PanelEmpleado extends PanelComponente {
 	public PanelEmpleado(Object[] cabeceras, Object[][] datos, String titulo) {
 		super(cabeceras, datos, titulo);
 	}
+	@Autowired
 	private EmpresaService empresaService;
+	@Autowired
 	private EmpleadoService empleadoService;
-	private JComboBox<String> selectionModeComboBox;
+	@Autowired
+	private DatosPersonalesService datosPersonalesService;
+	@Autowired
+	private DatoLaboralService datoLaboralService;
+	
+	private JComboBox<String> jComboBoxEmpresas;
+	
+	private JComboBox<String> jComboBoxDatosPersonales;
+	
+	private JComboBox<String> jComboBoxDatosLaborales;
 	
 	private Object[] cabeceras;
 	
@@ -42,25 +58,44 @@ public class PanelEmpleado extends PanelComponente {
 	public void alta() {
 		
 		DefaultTableModel defaultTableModel = (DefaultTableModel) getTabla().getModel();
-		 Object[] datos = {"0", ""};
+		 Object[] datos = {"0", "","","","","",""};
 		 defaultTableModel.addRow(datos);
 		 
   
-		 selectionModeComboBox = new JComboBox<String>() { 
-             public Dimension getMaximumSize() { 
-                 return getPreferredSize(); 
-             } 
-         }; 
+		 jComboBoxEmpresas = new JComboBox<String>();
         
          List <Empresa> nombreArrayEmpresa = (List<Empresa>) getEmpresaService().findAll();
         
          for (Empresa empresa : nombreArrayEmpresa) {
-        	 selectionModeComboBox.addItem(empresa.getNombre());
-          
+        	 jComboBoxEmpresas.addItem(empresa.getNombre());
          }
+         
+         getTabla().getColumnModel().getColumn(5).setCellEditor(new DefaultCellEditor(jComboBoxEmpresas));
+         
+         jComboBoxDatosPersonales = new JComboBox<String>();
+         
+         List <DatoPersonal> nombreArrayDatosPersonales = (List<DatoPersonal>) getDatosPersonalesService().findAll();
         
-        getTabla().getColumnModel().getColumn(5).setCellEditor(new DefaultCellEditor(selectionModeComboBox));
+         for (DatoPersonal datoPersonal : nombreArrayDatosPersonales) {
+        	 jComboBoxDatosPersonales.addItem(datoPersonal.getEstadoCivil().getDescripcion() + " - "
+ 					+ datoPersonal.getHijo().getChicos() + " - "
+ 					+ datoPersonal.getHijo().getChicas());
+         }
+
+        getTabla().getColumnModel().getColumn(6).setCellEditor(new DefaultCellEditor(jComboBoxDatosPersonales));
+        
+        jComboBoxDatosLaborales = new JComboBox<String>();
+        
+        List <DatoLaboral> nombreArrayDatosLaborales = (List<DatoLaboral>) getDatoLaboralService().findAll();
+       
+        for (DatoLaboral datoLaboral : nombreArrayDatosLaborales) {
+        	jComboBoxDatosLaborales.addItem(datoLaboral.getCargo().getDescripcion() + " - "
+					+ datoLaboral.getSalario());
+        }
+
+       getTabla().getColumnModel().getColumn(7).setCellEditor(new DefaultCellEditor(jComboBoxDatosLaborales));
 	}
+	
 
 	@Override
 	public void baja() {
@@ -69,12 +104,14 @@ public class PanelEmpleado extends PanelComponente {
 		 DefaultTableModel defaultTableModel=(DefaultTableModel) getTabla().getModel();
 		defaultTableModel.removeRow(getTabla().getSelectedRow());
 		
-		
 	}
 
 	@Override
 	public void modificar() {
-		// TODO Auto-generated method stub
+		int id = Integer.parseInt((String) getTabla().getModel().getValueAt(getTabla().getSelectedRow(), 0)); 
+		getEmpleadoService().save(null);
+		 DefaultTableModel defaultTableModel=(DefaultTableModel) getTabla().getModel();
+		defaultTableModel.removeRow(getTabla().getSelectedRow());
 
 	}
 
