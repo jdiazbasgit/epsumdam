9ab96e31169d1cd9440cb76e14a51357ef0258ef
Your Name <you@example.com>
2024-05-13 08:58:49 +0200

    s

--- src/main/java/epsum/curso/conexiondatos/ventanas/PanelDatosLaborales.java
+++ src/main/java/epsum/curso/conexiondatos/ventanas/PanelDatosLaborales.java
@@ -1,13 +1,20 @@
 package epsum.curso.conexiondatos.ventanas;
 
+import java.util.List;
+
+import javax.swing.DefaultCellEditor;
+import javax.swing.JComboBox;
 import javax.swing.table.DefaultTableModel;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import epsum.curso.conexiondatos.entidades.DatoLaboral;
+import epsum.curso.conexiondatos.entidades.Cargo;
+import epsum.curso.conexiondatos.servicios.CargoService;
 import epsum.curso.conexiondatos.servicios.DatoLaboralService;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import javax.swing.JComboBox;
 
 @Data
 @EqualsAndHashCode(callSuper=false)
@@ -23,34 +30,55 @@ public class PanelDatosLaborales extends PanelComponente {
     private String titulo;
 
     private int i;
+    
+    @Autowired
+	private CargoService cargoService;
 
+	private JComboBox<String> jComboBoxCargo;
+	
+
+
+    
     public PanelDatosLaborales(Object[] cabeceras, Object[][] datos, String titulo) {
         super(cabeceras, datos, titulo);
-        // TODO Auto-generated constructor stub
+
     }
 
      @Override
         public void alta() {
          DefaultTableModel defaultTableModel = (DefaultTableModel) getTabla().getModel();
-         Object[] datos = {"0", ""};
+         Object[] datos = {"0", "", ""};
          defaultTableModel.addRow(datos);
+         
+         
+         jComboBoxCargo = new JComboBox<String>();
+         
+         List <Cargo> cargos = (List<Cargo>) getCargoService().findAll();
+        
+         for (Cargo cargo : cargos) {
+        	 jComboBoxCargo.addItem(cargo.getDescripcion());
+         }
+         
+         getTabla().getColumnModel().getColumn(2).setCellEditor(new DefaultCellEditor(jComboBoxCargo));
+         
         }
+     
     @Override
     public void baja() {
         int id = Integer.parseInt((String) getTabla().getModel().getValueAt(getTabla().getSelectedRow(), 0));
         getDatoLaboralService().deleteById(id);
-         DefaultTableModel defaultTableModel=(DefaultTableModel) getTabla().getModel();
+        DefaultTableModel defaultTableModel=(DefaultTableModel) getTabla().getModel();
         defaultTableModel.removeRow(getTabla().getSelectedRow());
     }
 
     @Override
     public void modificar() {
         for (int i = 0; i < getTabla().getModel().getRowCount(); i++) {
-            DatoLaboral datolaboral = new  DatoLaboral();
-            datolaboral.setId(Integer.parseInt((String)getTabla().getModel().getValueAt(i, 0)));
-            datolaboral.setSalario(Integer.parseInt((String)getTabla().getModel().getValueAt(i, 1)));
-            datolaboral.getCargo().setDescripcion((String)getTabla().getModel().getValueAt(i, 2));
-            getDatoLaboralService().save(datolaboral);
+            DatoLaboral datoLaboral = new  DatoLaboral();
+            datoLaboral.setId(Integer.parseInt((String)getTabla().getModel().getValueAt(i, 0)));
+            datoLaboral.setSalario(Integer.parseInt((String)getTabla().getModel().getValueAt(i, 1)));
+            datoLaboral.getCargo().setDescripcion((String)getTabla().getModel().getValueAt(i, 2));
+            getDatoLaboralService().save(datoLaboral);
         }
     }
 
