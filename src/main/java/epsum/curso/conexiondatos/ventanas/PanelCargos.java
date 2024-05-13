cb444cf3ba79d3f29e3aafc75e5a431d0104d567
Your Namecls <you@example.com>
2024-05-13 08:39:13 +0200

    up

--- src/main/java/epsum/curso/conexiondatos/ventanas/PanelCargos.java
+++ src/main/java/epsum/curso/conexiondatos/ventanas/PanelCargos.java
@@ -4,6 +4,7 @@ import javax.swing.table.DefaultTableModel;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import epsum.curso.conexiondatos.entidades.Cargo;
 import epsum.curso.conexiondatos.servicios.CargoService;
 import lombok.Data;
 
@@ -30,10 +31,14 @@ public class PanelCargos extends PanelComponente {
 
 	@Override
 	public void baja() {
-		int id = Integer.parseInt((String) getTabla().getModel().getValueAt(getTabla().getSelectedRow(), 0));
+		/*int id = Integer.parseInt((String) getTabla().getModel().getValueAt(getTabla().getSelectedRow(), 0));
 		getCargoService().deleteById(id);
 		 DefaultTableModel defaultTableModel=(DefaultTableModel) getTabla().getModel();
-		defaultTableModel.removeRow(getTabla().getSelectedRow());
+		defaultTableModel.removeRow(getTabla().getSelectedRow());*/
+		Cargo cargo= new Cargo();
+		cargo.setDescripcion("ejemplo cambiado");
+		getCargoService().save(cargo);
+		System.out.println("a");
 	}
 
 	@Override
