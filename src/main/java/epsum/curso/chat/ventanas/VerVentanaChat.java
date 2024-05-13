7794150a2ac74a71c08a0894108bec601f8047f6
Rodrigo <rdc.1985@gmail.com>
2024-05-13 13:23:29 +0200

    sadasd

--- src/main/java/epsum/curso/chat/ventanas/VerVentanaChat.java
+++ src/main/java/epsum/curso/chat/ventanas/VerVentanaChat.java
@@ -1,20 +1,12 @@
 package epsum.curso.chat.ventanas;
 
 import java.awt.Frame;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-
-import epsum.curso.chat.ventanas.clientes.ClienteChat;
-import epsum.curso.chat.ventanas.servidores.LevantarServidor;
-import epsum.curso.chat.ventanas.servidores.ServidorChat;
+import org.springframework.context.annotation.Import;
+import epsum.curso.chat.ventanas.configuration.ClienteConfiguration;
 import epsum.curso.chat.ventanas.servidores.ServidorEscuchaMensajeCliente;
 import epsum.curso.chat.ventanas.servidores.ServidorEscuchaRegistroCliente;
 import epsum.curso.chat.ventanas.servidores.ServidorEscuchaSolicitudPrivado;
@@ -22,20 +14,23 @@ import lombok.Data;
 
 @SpringBootApplication
 @Data
+@Import(ClienteConfiguration.class)
 public class VerVentanaChat extends Frame implements CommandLineRunner {
 
+	@Autowired
 	private VentanaChat ventanaChat;
+	
+	@Autowired
+	private ServidorEscuchaRegistroCliente servidorEscuchaRegistroCliente;
+	
+	@Autowired
+	private ServidorEscuchaMensajeCliente servidorEscuchaMensajeCliente;
+	
+	@Autowired
+	private ServidorEscuchaSolicitudPrivado servidorEscuchaSolicitudPrivado;
+	
 	public static void main(String[] args) {
-		
-		ServidorEscuchaRegistroCliente servidorEscuchaRegistroCliente= 
-				new  ServidorEscuchaRegistroCliente(ClienteChat.PUERTO_EXCUCHA_CLIENTE_REGISTRO, ventana);
-		servidorEscuchaRegistroCliente.start();
-		ServidorEscuchaMensajeCliente servidorMensaje= new ServidorEscuchaMensajeCliente(ClienteChat.PUERTO_EXCUCHA_CLIENTE_MENSAJE, ventana);
-		servidorMensaje.start();
-		ServidorEscuchaSolicitudPrivado servidorEscuchaSolicitudPrtivado= new ServidorEscuchaSolicitudPrivado(ClienteChat.PUERTO_EXCUCHA_CLIENTE_PRIVADO_ALTA, ventana);
-		servidorEscuchaSolicitudPrtivado.start();
-		
-		SpringApplicationBuilder builder = new SpringApplicationBuilder(LevantarServidor.class);
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(VerVentanaChat.class);
 		builder.headless(false);
 		builder.run(args);
 		
@@ -45,7 +40,9 @@ public class VerVentanaChat extends Frame implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		getVentanaChat().setVisible(true);
-		
+		getServidorEscuchaRegistroCliente().start();
+		getServidorEscuchaMensajeCliente().start();
+		getServidorEscuchaSolicitudPrivado().start();
 	}
 
 	
--- src/main/java/epsum/curso/chat/ventanas/servidores/LevantarServidor.java
+++ src/main/java/epsum/curso/chat/ventanas/servidores/LevantarServidor.java
@@ -6,13 +6,23 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
+import epsum.curso.chat.ventanas.configuration.ServidorConfiguration;
 import lombok.Data;
 
 @SpringBootApplication
 @Data
+@Import(ServidorConfiguration.class)
 public class LevantarServidor implements CommandLineRunner {
 
+	@Autowired
+	private ServidorEscuchaRegistroServidor servidorEscuchaRegistroServidor;
+	@Autowired
+	private ServidorEscuchaBajaServidor servidorEscuchaBajaServidor;
+	@Autowired
+	private ServidorEscuchaMensajeServidor servidorEscuchaMensajeServidor;
+	
 	public static void main(String[] args) {
 		SpringApplicationBuilder builder = new SpringApplicationBuilder(LevantarServidor.class);
 		builder.headless(false);
@@ -21,31 +31,12 @@ public class LevantarServidor implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		servidorEscuchaRegistroServidor().start();
-		servidorEscuchaBajaServidor().start();
-		servidorEscuchaMensajeServidor().start();
+		getServidorEscuchaRegistroServidor().start();
+		getServidorEscuchaBajaServidor().start();
+		getServidorEscuchaMensajeServidor().start();
 
 	}
 
-	@Bean
-	public ServidorEscuchaRegistroServidor servidorEscuchaRegistroServidor() {
-		ServidorEscuchaRegistroServidor servidorEscuchaRegistroServidor = new ServidorEscuchaRegistroServidor(
-				ServidorChat.PUERTO_ESCUCHA_SERVIDOR_REGISTRO);
-		return servidorEscuchaRegistroServidor;
-	}
-
-	@Bean
-	public ServidorEscuchaBajaServidor servidorEscuchaBajaServidor() {
-		ServidorEscuchaBajaServidor servidorEscuchaBajaServidor = new ServidorEscuchaBajaServidor(
-				ServidorChat.PUERTO_ESCUCHA_SERVIDOR_BAJA);
-		return servidorEscuchaBajaServidor;
-	}
-
-	@Bean
-	public ServidorEscuchaMensajeServidor servidorEscuchaMensajeServidor() {
-		ServidorEscuchaMensajeServidor servidorEscuchaMensajeServidor = new ServidorEscuchaMensajeServidor(
-				ServidorChat.PUERTO_ESCUCHA_SERVIDOR_MENSAJE);
-		return servidorEscuchaMensajeServidor;
-	}
+	
 
 }
