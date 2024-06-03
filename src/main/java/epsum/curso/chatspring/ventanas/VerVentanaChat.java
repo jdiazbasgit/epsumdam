ae8d20fce8d0148a2073f4f4ad39a940407d8af4
Your Name <you@example.com>
2024-06-03 09:41:01 +0200

    a

--- src/main/java/epsum/curso/chatspring/ventanas/VerVentanaChat.java
+++ src/main/java/epsum/curso/chatspring/ventanas/VerVentanaChat.java
@@ -1,50 +1,102 @@
 package epsum.curso.chatspring.ventanas;
 
 import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Import;
-
-import epsum.curso.chatspring.configuracion.Configuracion;
 import epsum.curso.chatspring.ventanas.clientes.ClienteChat;
 import epsum.curso.chatspring.ventanas.servidores.ServidorChat;
 import epsum.curso.chatspring.ventanas.servidores.ServidorEscuchaMensajeCliente;
 import epsum.curso.chatspring.ventanas.servidores.ServidorEscuchaRegistroCliente;
 import epsum.curso.chatspring.ventanas.servidores.ServidorEscuchaSolicitudPrivado;
-import lombok.Data;
 
-
-@SpringBootApplication
-@Data
-@Import(Configuracion.class)
-public class VerVentanaChat extends Frame implements CommandLineRunner {
-	
-	@Autowired
-	private VentanaChat ventanaChat;
-	
-	@Autowired
-	private static ServidorEscuchaRegistroCliente servidorEscuchaRegistroCliente;
-	@Autowired
-	private static ServidorEscuchaMensajeCliente servidorEscuchaMensajeCliente;
-	@Autowired
-	private static ServidorEscuchaSolicitudPrivado servidorEscuchaSolicitudPrivado;
-	
+public class VerVentanaChat extends Frame implements WindowListener, ActionListener, KeyListener {
 
 	public static void main1(String[] args) {
-		SpringApplicationBuilder builder = new SpringApplicationBuilder(VerVentanaChat.class);
-		builder.headless(false);
-		builder.run(args);
+		// TODO Auto-generated method stub
+		VentanaChat ventana= new VentanaChat();
+		ventana.setVisible(true);
+		ServidorEscuchaRegistroCliente servidorEscuchaRegistroCliente= 
+				new  ServidorEscuchaRegistroCliente(ClienteChat.PUERTO_EXCUCHA_CLIENTE_REGISTRO, ventana);
+		servidorEscuchaRegistroCliente.start();
+		ServidorEscuchaMensajeCliente servidorMensaje= new ServidorEscuchaMensajeCliente(ClienteChat.PUERTO_EXCUCHA_CLIENTE_MENSAJE, ventana);
+		servidorMensaje.start();
+		ServidorEscuchaSolicitudPrivado servidorEscuchaSolicitudPrtivado= new ServidorEscuchaSolicitudPrivado(ClienteChat.PUERTO_EXCUCHA_CLIENTE_PRIVADO_ALTA, ventana);
+		servidorEscuchaSolicitudPrtivado.start();
+		
+		
+		
+		
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
-		ventanaChat.setVisible(true);
-		servidorEscuchaRegistroCliente.start();
-		servidorEscuchaMensajeCliente.start();
-		servidorEscuchaSolicitudPrivado.start();
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
