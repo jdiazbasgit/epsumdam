6dadc7f4662b14771a6f65b48567f8981e924b49
Your Namecls <you@example.com>
2024-06-06 09:31:49 +0200

    a

---
+++ src/main/java/epsum/curso/conexiondatos/DatosPrimitivos.java
@@ -0,0 +1,13 @@
+package epsum.curso.conexiondatos;
+
+public class DatosPrimitivos {
+
+	public static void main(String[] args) {
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);
+
+	}
+
+}
