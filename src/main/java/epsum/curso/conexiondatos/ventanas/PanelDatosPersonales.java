3ab597ee29acb86254c51a400f87bccf4a25cd88
DaniGN10 <daniel.galan@estudiante.epsum.school>
2024-06-06 11:14:41 +0200

    a

--- src/main/java/epsum/curso/conexiondatos/ventanas/PanelDatosPersonales.java
+++ src/main/java/epsum/curso/conexiondatos/ventanas/PanelDatosPersonales.java
@@ -1,4 +1,5 @@
 package epsum.curso.conexiondatos.ventanas;
+
 import java.util.List;
 
 import javax.swing.JComboBox;
@@ -25,94 +26,92 @@ public class PanelDatosPersonales extends PanelComponente {
 	public PanelDatosPersonales(String[] cabeceras, Object[][] datos, String titulo) {
 		super(cabeceras, datos, titulo);
 	}
+
 	@Autowired
 	private DatosPersonalesService datosPersonalesService;
-	
-	
-	
+
 	@Autowired
 	private EstadoCivilService estadoCivilService;
 	@Autowired
 	private HijoService hijoService;
-	
-	private JComboBox<EstadoCivil> jComboBoxEstadoCivil;
-	
-	private JComboBox<Hijo> jComboBoxHijo;
 
-	
+	private JComboBox<EstadoCivil> jComboBoxEstadoCivil;
+
+	private JComboBox<Hijo> jComboBoxHijo;
 
 	@Override
 	public void alta() {
-		int confirmation = JOptionPane.showConfirmDialog(null, "¿Deseas agregar un nuevo registro?", "Confirmacion",JOptionPane.YES_NO_OPTION);
-		if(confirmation == JOptionPane.YES_OPTION) {
-		try {
-			String descripcion = JOptionPane.showInputDialog(null,"Introduce la descripcion del nuevo cargo", "Nueva Descripción", JOptionPane.PLAIN_MESSAGE);
-			if (descripcion == null || descripcion.trim().isEmpty()) {
-				throw new IllegalArgumentException("La descripcion noo puede estar vacia");
-			}
-		DefaultTableModel defaultTableModel = (DefaultTableModel) getTabla().getModel();
-		jComboBoxEstadoCivil = new JComboBox<EstadoCivil>();
-		jComboBoxHijo = new JComboBox<Hijo>();
-		List<EstadoCivil> estadosCiviles = (List<EstadoCivil>) getEstadoCivilService().findAll();
-		List<Hijo> hijos = (List<Hijo>) getHijoService().findAll();
-		for (EstadoCivil estadoCivil : estadosCiviles ) {
-			jComboBoxEstadoCivil.addItem(estadoCivil);
+		int confirmation = JOptionPane.showConfirmDialog(null, "¿Deseas agregar un nuevo registro?", "Confirmacion",
+				JOptionPane.YES_NO_OPTION);
+		if (confirmation == JOptionPane.YES_OPTION) {
 
-	}
-		for (Hijo hijo : hijos) {
-			jComboBoxHijo.addItem(hijo);
+			DefaultTableModel defaultTableModel = (DefaultTableModel) getTabla().getModel();
+			jComboBoxEstadoCivil = new JComboBox<EstadoCivil>();
+			jComboBoxHijo = new JComboBox<Hijo>();
+			List<EstadoCivil> estadosCiviles = (List<EstadoCivil>) getEstadoCivilService().findAll();
+			List<Hijo> hijos = (List<Hijo>) getHijoService().findAll();
+			for (EstadoCivil estadoCivil : estadosCiviles) {
+				jComboBoxEstadoCivil.addItem(estadoCivil);
+
+			}
+			for (Hijo hijo : hijos) {
+				jComboBoxHijo.addItem(hijo);
+			}
+			Object[] datos = { "0", jComboBoxEstadoCivil, jComboBoxHijo };
+			defaultTableModel.addRow(datos);
+			defaultTableModel.setValueAt("Selecciona estado civil", defaultTableModel.getRowCount()-1, 1);
+			defaultTableModel.setValueAt("Selecciona hijos", defaultTableModel.getRowCount()-1, 2);
 		}
-		Object[] datos = { "0", jComboBoxEstadoCivil, jComboBoxHijo };
-		} catch (IllegalArgumentException e) {
-			JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);			
-		}
-		}
-		
+
 	}
 
 	@Override
 	public void baja() {
 		int id = Integer.parseInt((String) getTabla().getModel().getValueAt(getTabla().getSelectedRow(), 0));
-		int confirmation = JOptionPane.showConfirmDialog(null, "¿Estas seguro de borrar?"+getTabla().getModel().getValueAt(getTabla().getSelectedRow(),1)+"?");
+		int confirmation = JOptionPane.showConfirmDialog(null,
+				"¿Estas seguro de borrar?" + getTabla().getModel().getValueAt(getTabla().getSelectedRow(), 1) + "?");
 		if (confirmation == JOptionPane.YES_OPTION) {
-			try {		
-			getDatosPersonalesService().deleteById(id);
-			JOptionPane.showMessageDialog(null, "Registro borrado correctamente", "Exito", JOptionPane.INFORMATION_MESSAGE);
-			DefaultTableModel defaultTableModel=(DefaultTableModel) getTabla().getModel();
-			defaultTableModel.removeRow(getTabla().getSelectedRow());
-		}catch (DataIntegrityViolationException e) {
-			e.printStackTrace();
-			JOptionPane.showMessageDialog(null, "Registro no se ha podido borrar porque está en uso","Éxito", JOptionPane.INFORMATION_MESSAGE);
-		}catch(EmptyResultDataAccessException e) {
-			e.printStackTrace();
-			JOptionPane.showMessageDialog(null, "El registro no exsiste, se va a eliminar","Error", JOptionPane.INFORMATION_MESSAGE);
-			DefaultTableModel defaultTableModel=(DefaultTableModel) getTabla().getModel();
-			defaultTableModel.removeRow(getTabla().getSelectedRow());
-		}
+			try {
+				getDatosPersonalesService().deleteById(id);
+				JOptionPane.showMessageDialog(null, "Registro borrado correctamente", "Exito",
+						JOptionPane.INFORMATION_MESSAGE);
+				DefaultTableModel defaultTableModel = (DefaultTableModel) getTabla().getModel();
+				defaultTableModel.removeRow(getTabla().getSelectedRow());
+			} catch (DataIntegrityViolationException e) {
+				e.printStackTrace();
+				JOptionPane.showMessageDialog(null, "Registro no se ha podido borrar porque está en uso", "Éxito",
+						JOptionPane.INFORMATION_MESSAGE);
+			} catch (EmptyResultDataAccessException e) {
+				e.printStackTrace();
+				JOptionPane.showMessageDialog(null, "El registro no exsiste, se va a eliminar", "Error",
+						JOptionPane.INFORMATION_MESSAGE);
+				DefaultTableModel defaultTableModel = (DefaultTableModel) getTabla().getModel();
+				defaultTableModel.removeRow(getTabla().getSelectedRow());
+			}
 		}
 	}
-	
 
 	@Override
 	public void modificar() {
 		try {
-		for (int i = 0; i < getTabla().getModel().getRowCount(); i++) {
-			DatoPersonal datoPersonal = new DatoPersonal();
-			datoPersonal.setId(Integer.parseInt((String) getTabla().getModel().getValueAt(i, 0)));
-			datoPersonal.setEstadoCivil((EstadoCivil) getTabla().getModel().getValueAt(i, 1));
-			datoPersonal.setHijo((Hijo) getTabla().getModel().getValueAt(i, 2));
-			getDatosPersonalesService().save(datoPersonal);
-			DefaultTableModel defaultTableModel = (DefaultTableModel) getTabla().getModel();
-			defaultTableModel.setValueAt(String.valueOf(datoPersonal.getId()), i, 0);
-			
-		}
-		JOptionPane.showMessageDialog(null, "Las modificaciones fueron exitosas", "Éxito", JOptionPane.INFORMATION_MESSAGE);
+			for (int i = 0; i < getTabla().getModel().getRowCount(); i++) {
+				DatoPersonal datoPersonal = new DatoPersonal();
+				datoPersonal.setId(Integer.parseInt((String) getTabla().getModel().getValueAt(i, 0)));
+				datoPersonal.setEstadoCivil((EstadoCivil) getTabla().getModel().getValueAt(i, 1));
+				datoPersonal.setHijo((Hijo) getTabla().getModel().getValueAt(i, 2));
+				getDatosPersonalesService().save(datoPersonal);
+				DefaultTableModel defaultTableModel = (DefaultTableModel) getTabla().getModel();
+				defaultTableModel.setValueAt(String.valueOf(datoPersonal.getId()), i, 0);
+
+			}
+			JOptionPane.showMessageDialog(null, "Las modificaciones fueron exitosas", "Éxito",
+					JOptionPane.INFORMATION_MESSAGE);
 		} catch (Exception e) {
-	        JOptionPane.showMessageDialog(null, "Hubo un problema al modificar los datos", "Error", JOptionPane.ERROR_MESSAGE);
-	        e.printStackTrace(); 
-	    }
-			
+			JOptionPane.showMessageDialog(null, "Hubo un problema al modificar los datos", "Error",
+					JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+
 	}
 
 }
-
