eb420a64a6222a4023fc6e93c722d753e3e41278
jdiazbasgit <git@studiotapal.es>
2024-06-26 18:08:41 +0200

    a

---
+++ src/main/java/com/example/demo/controladores/ComisionesRestController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controladores;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.example.demo.entidades.Comision;
+import com.example.demo.repositorios.ComisionesCrudRepository;
+
+import lombok.Data;
+
+
+public class ComisionesRestController {
+	
+	@Autowired
+	private ComisionesCrudRepository comisionesCrudRepository;
+
+	public ComisionesCrudRepository getComisionesCrudRepository() {
+		return comisionesCrudRepository;
+	}
+
+	public void setComisionesCrudRepository(ComisionesCrudRepository comisionesCrudRepository) {
+		this.comisionesCrudRepository = comisionesCrudRepository;
+	}
+
+	@CrossOrigin(origins ="*" )
+	public List<Comision> getComisiones(){
+		return (List<Comision>) getComisionesCrudRepository().findAll();
+	}
+
+	public ComisionesRestController getComisionesRestController() {
+		return comisionesRestController;
+	}
+
+	public void setComisionesRestController(ComisionesRestController comisionesRestController) {
+		this.comisionesRestController = comisionesRestController;
+	}
+	
+}
