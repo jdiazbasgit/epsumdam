676c075259ddf7d93ccbd0c816f88c110c9078f5
jdiazbasgit <git@studiotapal.es>
2024-06-26 17:59:38 +0200

    a

--- src/main/java/com/example/demo/repositorios/ComisionesCrudRepository.java
+++ src/main/java/com/example/demo/repositorios/ComisionesCrudRepository.java
@@ -3,7 +3,8 @@ package com.example.demo.repositorios;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.entidades.Articulo;
+import com.example.demo.entidades.Comision;
 
-public interface ComisionesCrudRepository extends CrudRepository<Articulo, Integer> {
+public interface ComisionesCrudRepository extends CrudRepository<Comision, Integer> {
 
 }
