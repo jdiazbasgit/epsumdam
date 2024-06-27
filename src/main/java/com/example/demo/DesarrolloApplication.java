939da2b480da707d591eb73488e9239432553ce3
Javito <fj.diazb@gmail.com>
2024-06-27 14:57:43 +0200

    a

--- src/main/java/com/example/demo/DesarrolloApplication.java
+++ src/main/java/com/example/demo/DesarrolloApplication.java
@@ -7,19 +7,17 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class DesarrolloApplication implements WebMvcConfigurer{
+public class DesarrolloApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesarrolloApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer1() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/empleados").allowedOrigins("*");
-			}
-		};
+
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/empleados").allowedOrigins("*");
 	}
+
 }
