4aeec5f7c699493dd49ae525d2d0ee7497ac976e
Javito <fj.diazb@gmail.com>
2024-06-27 13:23:48 +0200

    a

--- src/main/java/com/example/demo/DesarrolloApplication.java
+++ src/main/java/com/example/demo/DesarrolloApplication.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DesarrolloApplication {
@@ -10,4 +13,13 @@ public class DesarrolloApplication {
 		SpringApplication.run(DesarrolloApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("*");
+			}
+		};
+	}
 }
