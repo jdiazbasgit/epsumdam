8deea1ba1bbe26bc613d10a43672703b15e2dcf4
Javito <fj.diazb@gmail.com>
2024-06-27 13:25:52 +0200

    a

--- src/main/java/com/example/demo/DesarrolloApplication.java
+++ src/main/java/com/example/demo/DesarrolloApplication.java
@@ -18,7 +18,7 @@ public class DesarrolloApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*").allowedOrigins("*");
+				registry.addMapping("/empleados").allowedOrigins("*");
 			}
 		};
 	}
