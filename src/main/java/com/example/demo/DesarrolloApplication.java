4cfbc6f92306f750c79778d5b329733203b41231
Javito <fj.diazb@gmail.com>
2024-06-27 15:08:11 +0200

    a

--- src/main/java/com/example/demo/DesarrolloApplication.java
+++ src/main/java/com/example/demo/DesarrolloApplication.java
@@ -2,11 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableWebMvc
 public class DesarrolloApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
